61b42b922d017e9fdcd91f71dbb1758dabae1f14
Matheus Baron <matheusbaron10@gmail.com>
2025-05-05 17:04:14 -0300

    feat: [API] create create-project route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -27,6 +27,7 @@ import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
+import { createProject } from './routes/projects/create-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -82,6 +83,8 @@ app.register(createOrganization)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+
+app.register(createProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`Server is running on http://localhost:${env.SERVER_PORT}`)
