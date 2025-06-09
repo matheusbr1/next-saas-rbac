ea88f7a8f05512994febd6479f92140319ed675f
Matheus <mbribeiro@stefanini.com>
2025-06-09 19:47:57 -0300

    feat: [api] cria rota update-member

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -33,6 +33,7 @@ import { getProject } from './routes/projects/get-project'
 import { getProjects } from './routes/projects/get-projects'
 import { updateProject } from './routes/projects/update-project'
 import { getMembers } from './routes/members/get-members'
+import { updateMember } from './routes/members/update-member'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -96,6 +97,7 @@ app.register(getProjects)
 app.register(updateProject)
 
 app.register(getMembers)
+app.register(updateMember)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`Server is running on http://localhost:${env.SERVER_PORT}`)
