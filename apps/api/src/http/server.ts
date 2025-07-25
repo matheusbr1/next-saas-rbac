4884743fc0c1ead24eb31df430312e3b09c13f32
Matheus <mbribeiro@stefanini.com>
2025-07-24 21:03:34 -0300

    feat: [api] cria rota reject-invites

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -38,6 +38,8 @@ import { removeMember } from './routes/members/remove-member'
 import { createInvite } from './routes/invite/create-invite'
 import { getInvite } from './routes/invite/get-invite'
 import { getInvites } from './routes/invite/get-invites'
+import { acceptInvite } from './routes/invite/accept-invite'
+import { rejectInvite } from './routes/invite/reject-invite'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -108,6 +110,7 @@ app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
 app.register(acceptInvite)
+app.register(rejectInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log(`Server is running on http://localhost:${env.SERVER_PORT}`)
